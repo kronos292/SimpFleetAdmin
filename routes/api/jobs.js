aaa2aee0ecdcd0d406c2e1679014993362748a3f
Rosemary18 <zulkarnain.ahmad18@gmail.com>
2019-08-22 14:25:02 +0700

    edit job route

--- routes/api/jobs.js
+++ routes/api/jobs.js
@@ -4,6 +4,7 @@ const router = express.Router();
 /* load model needed*/
 const Job = require("../../models/Job");
 const User = require("../../models/User");
+const Vessel = require("../../models/Vessel");
 
 router.get("/test", (req, res) => res.json("Job Working"));
 
