b8f41eb302ce32790fbdf938c0f5ec6762f745f2
S E N I N  S E L A S A <53807938+Rosemary18@users.noreply.github.com>
2019-09-11 21:22:47 +0700

    Make folder on component and modified footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,88 +1,191 @@
 import React from "react";
 
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Nav from "react-bootstrap/Nav";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faWhatsapp,
+  faFacebook,
+  faInstagram,
+  faWeixin,
+  faLinkedin,
+  faTelegram
+} from "@fortawesome/free-brands-svg-icons";
+
+import MediaQuery from "react-responsive";
+
+import "./Footer.css";
 export default function Footer() {
   return (
-    <div className="footer">
-      <div className="container">
-        <div className="row">
-          <div className="col-6 col-lg-3">
-            <h4 className="text-light">Company</h4>
-            <a
-              className="text-light"
-              href="https://www.shipsuppliesdirect.com/"
-              target="_blank"
-            >
-              About Ship Supplies Direct
-            </a>
-            <br />
-            <a
-              className="text-light"
-              href="https://medium.com/@shipsuppliesdirect"
-              target="_blank"
-            >
-              Blog
-            </a>
-            <br />
-            <a className="text-light" href="/">
-              Press
-            </a>
-          </div>
-          <div className="col-6 col-lg-3">
-            <h4 className="text-light">SimpFleet</h4>
-            <a className="text-light" href="/" target="_blank">
-              FAQ
-            </a>
-            <br />
-            <a className="text-light" href="/" target="_blank">
-              Career
-            </a>
-          </div>
-        </div>
-        <br />
-        <br />
-        <div className="dropdown-divider" />
-        <div className="row">
-          <div className="col-12 col-lg-6">
-            <h6 className="text-light">&copy;2019 Ship Supplies Direct</h6>
-          </div>
-          <div className="col-12 col-lg-6 text-right">
-            <a
-              className="text-light"
-              href="https://api.whatsapp.com/send?phone=6587480467"
-              target="_blank"
-            >
-              <i className="fab fa-whatsapp fa-2x" />
-            </a>
-            <a
-              className="text-light"
-              href="https://www.facebook.com/shipsuppliesdirect"
-              target="_blank"
-            >
-              <i className="fab fa-facebook fa-2x" />
-            </a>
-            <a
-              className="text-light"
-              href="https://www.instagram.com/shipsuppliesdirect/s"
-              target="_blank"
-            >
-              <i className="fab fa-instagram fa-2x" />
-            </a>
-            <a
-              className="text-light"
-              href="https://www.linkedin.com/company/ship-supplies-direct/"
-              target="_blank"
-            >
-              <i className="fab fa-linkedin fa-2x" />
-            </a>
-            <a
-              className="text-light"
-              href="https://telegram.me/ShipSuppliesDirect"
-              target="_blank"
-            >
-              <i className="fab fa-telegram fa-2x" />
-            </a>
-          </div>
-        </div>
+    <div id="footer-div">
+      <div className="section-footer">
+        <Container>
+          <Row className="nav-btm">
+            <Col xs={6} md={{ span: 3, offset: 0 }}>
+              <Nav defaultActiveKey="#home" className="flex-column">
+                <Nav.Link className="disabled title-footer">Company</Nav.Link>
+                <Nav.Link
+                  href="https://www.shipsuppliesdirect.com/"
+                  target="_blank"
+                >
+                  About Ship Supplies Direct
+                </Nav.Link>
+                <Nav.Link href="https://medium.com/@shipsuppliesdirect">
+                  Blog
+                </Nav.Link>
+                <Nav.Link href="/">Press</Nav.Link>
+              </Nav>
+            </Col>
+            {/*<Col xs={6} md={{ span: 3, offset: 0 }}>*/}
+            {/*    <Nav defaultActiveKey="#home" className="flex-column">*/}
+            {/*        <Nav.Link className='disabled title-footer'>SimpFleet</Nav.Link>*/}
+            {/*        <Nav.Link href="/" target="_blank">FAQ</Nav.Link>*/}
+            {/*        <Nav.Link href="/" target="_blank">Career</Nav.Link>*/}
+            {/*    </Nav>*/}
+            {/*</Col>*/}
+          </Row>
+          <Row>
+            <hr className="line-footer" />
+          </Row>
+          <MediaQuery query="(min-width: 426px)">
+            <Row className="font-large">
+              <Col>
+                <h6 className="h6-footer">©2019 Ship Supplies Direct</h6>
+              </Col>
+              <Col
+                md={{ span: 3, offset: 0 }}
+                className="d-flex justify-content-end align-items-start"
+              >
+                <a
+                  href="https://api.whatsapp.com/send?phone=6587480467"
+                  target="_blank"
+                  className="sicon"
+                >
+                  <FontAwesomeIcon
+                    icon={faWhatsapp}
+                    color="white"
+                    className="highlight"
+                  />
+                </a>
+                <a
+                  href="https://www.facebook.com/SimpFleet-2427717804140333/"
+                  target="_blank"
+                  className="sicon"
+                >
+                  <FontAwesomeIcon
+                    icon={faFacebook}
+                    color="white"
+                    className="highlight"
+                  />
+                </a>
+                <a
+                  href="https://www.instagram.com/shipsuppliesdirect/"
+                  target="_blank"
+                  className="sicon"
+                >
+                  <FontAwesomeIcon
+                    icon={faInstagram}
+                    color="white"
+                    className="highlight"
+                  />
+                </a>
+                {/*<a href='#home' target='_blank' className='sicon'><FontAwesomeIcon icon={faWeixin} color="white" className='highlight'/></a>*/}
+                <a
+                  href="https://www.linkedin.com/company/ship-supplies-direct/"
+                  target="_blank"
+                  className="sicon"
+                >
+                  <FontAwesomeIcon
+                    icon={faLinkedin}
+                    color="white"
+                    className="highlight"
+                  />
+                </a>
+                <a
+                  href="https://telegram.me/ShipSuppliesDirect"
+                  target="_blank"
+                  className="sicon"
+                >
+                  <FontAwesomeIcon
+                    icon={faTelegram}
+                    color="white"
+                    className="highlight"
+                  />
+                </a>
+              </Col>
+            </Row>
+          </MediaQuery>
+          <MediaQuery query="(max-width: 425px)">
+            <Row className="font-large">
+              <Col className="d-flex justify-content-center align-items-start">
+                <h6 className="h6-footer">©2019 Ship Supplies Direct</h6>
+              </Col>
+            </Row>
+            <Row className="font-large">
+              <Col className="d-flex justify-content-center align-items-start">
+                <a
+                  href="https://api.whatsapp.com/send?phone=6587480467"
+                  target="_blank"
+                  className="sicon"
+                >
+                  <FontAwesomeIcon
+                    icon={faWhatsapp}
+                    color="white"
+                    className="highlight"
+                  />
+                </a>
+                <a
+                  href="https://www.facebook.com/SimpFleet-2427717804140333/"
+                  target="_blank"
+                  className="sicon"
+                >
+                  <FontAwesomeIcon
+                    icon={faFacebook}
+                    color="white"
+                    className="highlight"
+                  />
+                </a>
+                <a
+                  href="https://www.instagram.com/shipsuppliesdirect/"
+                  target="_blank"
+                  className="sicon"
+                >
+                  <FontAwesomeIcon
+                    icon={faInstagram}
+                    color="white"
+                    className="highlight"
+                  />
+                </a>
+                {/*<a href='#home' target='_blank' className='sicon'><FontAwesomeIcon icon={faWeixin} color="white" className='highlight' /></a>*/}
+                <a
+                  href="https://www.linkedin.com/company/ship-supplies-direct/"
+                  target="_blank"
+                  className="sicon"
+                >
+                  <FontAwesomeIcon
+                    icon={faLinkedin}
+                    color="white"
+                    className="highlight"
+                  />
+                </a>
+                <a
+                  href="https://telegram.me/ShipSuppliesDirect"
+                  target="_blank"
+                  className="sicon"
+                >
+                  <FontAwesomeIcon
+                    icon={faTelegram}
+                    color="white"
+                    className="highlight"
+                  />
+                </a>
+              </Col>
+            </Row>
+          </MediaQuery>
+        </Container>
       </div>
     </div>
   );
