466e0447975341fbf9ec2939e24a82501240eead
wiwie sanjaya <wiwiessd@gmail.com>
2019-10-09 10:42:10 +0700

    preparing for pulling

---
+++ client/src/components/UserApproval/UserApproval.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class UserApproval extends Component {
+  render() {
+    return (
+      <div>
+        <h1>UserApproval</h1>
+      </div>
+    );
+  }
+}
+export default UserApproval;
