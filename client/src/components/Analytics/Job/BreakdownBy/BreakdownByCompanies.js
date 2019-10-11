ae26ee77ebb0df6f19c5a4b092dbea42a19afd8c
Rosemary18 <zulkarnain.ahmad18@gmail.com>
2019-10-11 13:55:15 +0700

    fill dummy data to analitics table

--- client/src/components/Analytics/Job/BreakdownBy/BreakdownByCompanies.js
+++ client/src/components/Analytics/Job/BreakdownBy/BreakdownByCompanies.js
@@ -3,25 +3,65 @@ import { Row, Table, Col } from "reactstrap";
 
 class BreakdownByCompanies extends Component {
   render() {
-    return (
-      <Row>
-        <Col cs="12" md={{ size: 12, offset: 0 }} className="text-center">
-          <h1>Job breakdown by Companies</h1>
-        </Col>
-        <Col cs="12" md={{ size: 12, offset: 0 }}>
-          <Table striped hover bordered responsive>
-            <tr>
-              <th>Month</th>
-              <th>Companies</th>
-              <th>Ongoing Jobs</th>
-              <th>Completed Jobs</th>
-              <th>Cancelled Jobs</th>
-              <th>Total Jobs</th>
-            </tr>
-          </Table>
-        </Col>
-      </Row>
-    );
+    const { jobMonthCategory } = this.props;
+    switch (jobMonthCategory) {
+      case null:
+        return <div></div>;
+      default:
+        const jobCompaniesCategories = Object.keys(jobMonthCategory).map(
+          (key, index) => {
+            const jobs = jobMonthCategory[key];
+            const cancelledJobs = [];
+            const openJobs = [];
+            const completedJobs = [];
+            for (let i = 0; i < jobs.length; i++) {
+              const job = jobs[i];
+
+              if (job.isCancelled === "Confirmed") {
+                cancelledJobs.push(job);
+              } else {
+                if (job.jobTrackers.length === 6) {
+                  completedJobs.push(job);
+                } else {
+                  openJobs.push(job);
+                }
+              }
+            }
+
+            return (
+              <tr key={index}>
+                <td>{key}</td>
+                <td>-</td>
+                <td>{openJobs.length}</td>
+                <td>{completedJobs.length}</td>
+                <td style={{ color: "red" }}>{cancelledJobs.length}</td>
+                <td>{jobs.length}</td>
+              </tr>
+            );
+          }
+        );
+
+        return (
+          <Row>
+            <Col cs="12" md={{ size: 12, offset: 0 }} className="text-center">
+              <h1>Job breakdown by Companies</h1>
+            </Col>
+            <Col cs="12" md={{ size: 12, offset: 0 }}>
+              <Table striped hover bordered responsive>
+                <tr>
+                  <th>Month</th>
+                  <th>Companies</th>
+                  <th>Ongoing Jobs</th>
+                  <th>Completed Jobs</th>
+                  <th>Cancelled Jobs</th>
+                  <th>Total Jobs</th>
+                </tr>
+                {jobCompaniesCategories}
+              </Table>
+            </Col>
+          </Row>
+        );
+    }
   }
 }
 export default BreakdownByCompanies;
--- client/src/components/Analytics/Job/BreakdownBy/BreakdownByDeliveryLocations.js
+++ client/src/components/Analytics/Job/BreakdownBy/BreakdownByDeliveryLocations.js
@@ -3,27 +3,69 @@ import { Row, Table, Col } from "reactstrap";
 
 class BreakdownByDeliveryLocations extends Component {
   render() {
-    return (
-      <Row>
-        <Col cs="12" md={{ size: 12, offset: 0 }} className="text-center">
-          <h1>Job breakdown by Delivery Locations</h1>
-        </Col>
-        <Col cs="12" md={{ size: 12, offset: 0 }}>
-          <Table striped hover bordered responsive>
-            <tr>
-              <th>Month</th>
-              <th>Delivery Location</th>
-              <th>Items to Deliver</th>
-              <th>Items to Offland</th>
-              <th>Ongoing Jobs</th>
-              <th>Completed Jobs</th>
-              <th>Cancelled Jobs</th>
-              <th>Total Jobs</th>
+    const { jobMonthCategory } = this.props;
+    switch (jobMonthCategory) {
+      case null:
+        return <div></div>;
+      default:
+        const jobDeliveryLocationsCategories = Object.keys(
+          jobMonthCategory
+        ).map((key, index) => {
+          const jobs = jobMonthCategory[key];
+          const cancelledJobs = [];
+          const openJobs = [];
+          const completedJobs = [];
+          for (let i = 0; i < jobs.length; i++) {
+            const job = jobs[i];
+
+            if (job.isCancelled === "Confirmed") {
+              cancelledJobs.push(job);
+            } else {
+              if (job.jobTrackers.length === 6) {
+                completedJobs.push(job);
+              } else {
+                openJobs.push(job);
+              }
+            }
+          }
+
+          return (
+            <tr key={index}>
+              <td>{key}</td>
+              <td>-</td>
+              <td>-</td>
+              <td>-</td>
+              <td>{openJobs.length}</td>
+              <td>{completedJobs.length}</td>
+              <td style={{ color: "red" }}>{cancelledJobs.length}</td>
+              <td>{jobs.length}</td>
             </tr>
-          </Table>
-        </Col>
-      </Row>
-    );
+          );
+        });
+
+        return (
+          <Row>
+            <Col cs="12" md={{ size: 12, offset: 0 }} className="text-center">
+              <h1>Job breakdown by Delivery Locations</h1>
+            </Col>
+            <Col cs="12" md={{ size: 12, offset: 0 }}>
+              <Table striped hover bordered responsive>
+                <tr>
+                  <th>Month</th>
+                  <th>Delivery Location</th>
+                  <th>Items to Deliver</th>
+                  <th>Items to Offland</th>
+                  <th>Ongoing Jobs</th>
+                  <th>Completed Jobs</th>
+                  <th>Cancelled Jobs</th>
+                  <th>Total Jobs</th>
+                </tr>
+                {jobDeliveryLocationsCategories}
+              </Table>
+            </Col>
+          </Row>
+        );
+    }
   }
 }
 export default BreakdownByDeliveryLocations;
